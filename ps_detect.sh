8048d4e294c19c3b44f62a266d046582ee4526b8
Adam <aflickem@emich.edu>
2017-05-10 11:23:21 -0400

    Finished with firewalld and iptables

--- ps_detect.sh
+++ ps_detect.sh
@@ -1,39 +1,47 @@
 #!/bin/bash
 
-# A script to check if being port scanned. Creates appropriate rule for banning the ip.
+# A script to check if being port scanned. Creates appropriate rule for banning the ip using firewalld or iptables.
 # Written by Adam Flickema
 # github.com/aflick2486
 # aflickem@emich.edu
 
 iptablesLog() {
-	iptables-save | grep -- "-A INPUT -j LOG --log-prefix \"Log: \""
+	iptables -C INPUT -p tcp -j LOG --log-prefix "Log: " &> /dev/null	#Checks if rule in iptables already
 	if [ $? != 0 ]; then
-		iptables -A INPUT -p tcp -j LOG --log-prefix "Log: "
+		iptables -A INPUT -p tcp -j LOG --log-prefix "Log: "			#If not then add it
 	else
-		:
+		:																#Else do nothing
 	fi
-	cmd=`sudo cat /var/log/messages | /bin/grep "Log: " | awk '{print $10}' | sed s/SRC=// > /root/ips.txt`
-	declare -A ips
+	cmd=`sudo cat /var/log/messages | /bin/grep "Log: " | awk '{print $10}' | sed s/SRC=// > /root/ips.txt`	#Grab all ips from /var/log/messages
+	declare -A ips														#Declares an associative array
 	file='/root/ips.txt'
 	while read line; do
-		ip=$(echo "$line" | cut -f1)
+		ip=$(echo "$line" | cut -f1)									#Parses each line in /root/ips.txt
 		if test "${ips[$ip]+isset}" ; then
-			((ips[$ip]++))
+			((ips[$ip]++))											#If already in the associative array, increase the value by 1 for each seen
 		else
-			ips[$ip]=1
+			ips[$ip]=1													#If first time seen, add ip as a key
 		fi
-	done < ${file}
+	done < ${file}														#Close the file
 	for key in "${!ips[@]}"; do
-		if [ ${ips[$key]} -ge '20' ] ; then
-			iptables-save | grep -- "-A INPUT -s $key -j DROP" &> /dev/null
+		if [ ${ips[$key]} -ge '20' ] ; then								#If the value for the ip is more than 20
+			iptables -C INPUT -s $key -j DROP &> /dev/null				#Check if the ip is already banned
 			if [ $? != 0 ]; then
-				iptables -A INPUT -s $key -j DROP
+				iptables -A INPUT -s $key -j DROP						#If not then ban it
 			else
 				:
 			fi
 		else
-			return
+			:
 		fi
+	done
+	dt=`date '+%Y-%m-%d %H:%M:%S'`										#Grab the date and time to put in the log file
+	echo "$dt" >> /root/ps.log
+	for domain in ${!ips[@]}; do
+		echo "##################" >> /root/ps.log
+		echo "$domain" >> /root/ps.log									#Put each ip into the log file
+		`whois $domain >> /root/ps.log`									#Do a whois lookup on ip and put into the log file
+		echo "##################" >> /root/ps.log
 	done
 }
 
@@ -62,16 +70,23 @@ firewalldLog(){
 			:
 		fi
 	done
+	dt=`date '+%Y-%m-%d %H%M%S'`
+	echo "$dt" >> /root/ps.log
+	for domain in ${!ips[@]}; do
+		echo "##################" >> /root/ps.log
+		echo "$domain" >> /root/ps.log
+		`whois $domain >> /root/ps.log`			#Do a whois lookup on ip and put into the log file
+		echo "##################" >> /root/ps.log
+	done
 }
 
-
-# Determine which firewall installed
-if [ $(which iptables) ]; then
-	echo "" > /root/ips.txt
-	iptablesLog
-elif [ $(which firewalld) ]; then
-	echo "" > /root/ips.txt
-	firewalldLog
+# Determine which firewall installed, if both are installed uses iptables
+if [ $(which iptables) ]; then						#Checks if iptables is installed
+	echo "" > /root/ips.txt							#Clear the ips file
+	iptablesLog										#Execute the iptables subroutine
+elif [ $(which firewalld) ]; then					#Check if firewalld is installed
+	echo "" > /root/ips.txt							#Clear ips file
+	firewalldLog									#Execute the firewalld subroutine
 else
 	echo "No supported firewall. Exiting."
 	exit 0
